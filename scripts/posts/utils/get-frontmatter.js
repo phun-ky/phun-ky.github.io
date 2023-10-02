9e1b63bbdfffb1a0e0d178ace48f257330fb6f85
Alexander Vassbotn Røyne-Helgesen <alexander+github.com@phun-ky.net>
2023-10-02 14:54:21 +0200

    fix: 🐛 use html in description were needed

--- scripts/posts/utils/get-frontmatter.js
+++ scripts/posts/utils/get-frontmatter.js
@@ -2,6 +2,7 @@ import Markdoc from '@markdoc/markdoc';
 
 import { processTokens } from './process-tokens.js';
 import { parseMarkdocFrontmatter } from './parse-markdoc-frontmatter.js';
+import { getHTMLfromMarkdown } from './get-html-from-markdown.js';
 
 export const getFrontmatter = (text) => {
   const tokenizer = new Markdoc.Tokenizer({ html: true });
@@ -25,6 +26,8 @@ export const getFrontmatter = (text) => {
       frontmatter.slug = slug;
     }
   }
+
+  frontmatter.description = getHTMLfromMarkdown(frontmatter.description).replace('<article>','').replace('</article>','').replace('<p>','').replace('</p>','');
 
   return frontmatter;
 };
