44f160340e749843e226357005613a02784d5e15
Alexander Vassbotn Røyne-Helgesen <alexander+github.com@phun-ky.net>
2023-10-07 21:07:57 +0200

    refactor: 💡 Comment out copy / language feat for prism

--- scripts/posts/components/BodyScripts/index.js
+++ scripts/posts/components/BodyScripts/index.js
@@ -28,5 +28,45 @@ export const BodyScripts = () => {
         }
       })
     }
+
+    // const preElements = document.querySelectorAll('pre');
+
+    // if(preElements && preElements.length !== 0){
+    //   preElements.forEach(element => {
+    //     const attrs = Array.from(element.attributes).reduce((attrs,attribute) => {attrs[attribute.name] = attribute.value; return attrs;},{})
+    //     const { language } = attrs;
+
+    //     if(!language || language === '' || language === 'shell-session'){
+    //       return;
+    //     }
+
+    //     const toolsElement = document.createElement('span');
+    //     toolsElement.classList.add('ph');
+    //     toolsElement.classList.add('tools');
+
+    //     const languageElement = document.createElement('span');
+    //     languageElement.textContent = language;
+    //     languageElement.classList.add('ph');
+    //     languageElement.classList.add('language');
+    //     toolsElement.appendChild(languageElement);
+
+    //     const buttonElement = document.createElement('button');
+    //     buttonElement.textContent = 'copy';
+    //     buttonElement.classList.add('ph');
+    //     buttonElement.classList.add('copy');
+    //     toolsElement.appendChild(buttonElement);
+
+    //     buttonElement.addEventListener('click', e => {
+    //       const preElement = e.target.closest('pre');
+
+    //       if(preElement){
+    //         navigator.clipboard.writeText(preElement.textContent);
+    //       }
+    //     })
+
+    //     element.appendChild(toolsElement);
+
+    //   })
+    // }
   </script>`;
 };
